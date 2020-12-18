ec3685cc3bc841b5d683a908046ecc8f67861258
Alessandro Paolucci <alepao81@gmail.com>
2020-12-18 15:27:28 +0100

    update

--- app.js
+++ app.js
@@ -196,7 +196,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"start": 0, "audio": 0, "end": 33081902, "filename": "/multimedia.pak"}, {"start": 33081902, "audio": 0, "end": 33082926, "filename": "/data/data"}, {"start": 33082926, "audio": 0, "end": 33083950, "filename": "/data/data.bkp"}], "remote_package_size": 33083950, "package_uuid": "3bd887cc-828c-4840-b385-f56d493d72bc"});
+   loadPackage({"files": [{"start": 0, "audio": 0, "end": 33081902, "filename": "/multimedia.pak"}, {"start": 33081902, "audio": 0, "end": 33082926, "filename": "/data/data"}, {"start": 33082926, "audio": 0, "end": 33083950, "filename": "/data/data.bkp"}], "remote_package_size": 33083950, "package_uuid": "6ef47b6e-801a-4cba-a493-a057aeb0fdc3"});
   
   })();
   
